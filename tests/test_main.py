ef4cdd80b54fc721e15a598fe3caf0d3654b6142
HaoKann <baizhanovdamir@mail.ru>
2025-09-23 17:27:43 +0500

    Started implementing tests: created fixture and main_test files

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+
+# --- Тесты для корневых эндпоинтов ---
+
+def test_read_root(client: TestClient):
+    """
+    Проверяем, что главный эндпоинт ("/") работает и возвращает правильный статус.
+    (client: TestClient): Наш "виртуальный Postman", который мы создали в conftest.py.
+    Pytest автоматически найдет его и передаст сюда.
+    """
+
+    # Шаг 1: Отправляем GET-запрос на адрес "/"
+    response = client.get('/')
+
+    # Шаг 2: Проверяем результат (Утверждаем)
+    assert response.status_code == 200
+    assert response.json() == {'status': 'API is running.'}
+
+def test_not_found(client: TestClient):
+    """
+    Проверяем, что запрос к несуществующему пути возвращает ошибку 404 Not Found.
+    """
+    response = client.get('/a/b/c/d/e/f/g')
+    assert response.status_code == 404
