9cb1a0e1d3646b922c5ce9d7287ac6f4ee3d66a7
HaoKann <baizhanovdamir@mail.ru>
2026-01-13 11:35:44 +0500

    Feat: Add create_product mutation and get_product query by ID

--- graphql_app/schema.py
+++ graphql_app/schema.py
@@ -14,7 +14,9 @@ class ProductType:
     price: int
 
 
-# --- 2. Пишем Резолвер (функцию добычи данных) ---
+# --- 2. Резолверы
+
+# ЧТЕНИЕ
 # info: Info — это специальный параметр Strawberry, в нем лежит объект запроса
 async def get_products(info: Info) -> List[ProductType]:
     # 1. Достаем объект request из контекста Strawberry
@@ -43,7 +45,59 @@ async def get_products(info: Info) -> List[ProductType]:
             for row in rows
         ]
 
-# --- 3. Главный класс Query ---
+
+# ЧТЕНИЕ ОДНОГО ТОВАРА
+# Обрати внимание: возвращаем Optional[ProductType], так как товара может и не быть
+async def get_product(info: Info, product_id: int) -> Optional[ProductType]:
+    request = info.context['request']
+    pool = request.app.state.pool
+
+    if not pool:
+        raise Exception("Нет подключения к БД!")
+
+    async with pool.acquire() as connection:
+        # Используем WHERE id = $1
+        query = "SELECT id, name, description, price FROM products WHERE id = $1"
+        row = await connection.fetchrow(query, product_id)
+
+        if row:
+            return ProductType(
+                id=row["id"],
+                name=row["name"],
+                description=row["description"],
+                price=row["price"]
+            )
+        else:
+            return None # Если не нашли, возвращаем null
+
+
+# ЗАПИСЬ
+async def create_product(info: Info, name: str, price: int, description: Optional[str] = None) -> ProductType:
+   request = info.context['request']
+   pool = request.app.state.pool
+
+   if not pool:
+       raise Exception('Нет подключения к БД!')
+
+   async with pool.acquire() as connection:
+        # Мы делаем INSERT и сразу просим вернуть ID созданной строки (RETURNING id)
+        # Это фишка PostgreSQL, чтобы не делать два запроса.
+        query = """
+            INSERT INTO products (name, description, price)
+            VALUES ($1, $2, $3)
+            RETURNING id
+        """
+        # Используем fetchrow, так как ожидаем ровно одну строку ответа (id)
+        row = await connection.fetchrow(query, name, description, price)
+        new_id = row['id']
+
+        # Возвращаем созданный объект, чтобы клиент сразу увидел его ID
+        return ProductType(id=new_id, name=name, description=description, price=price)
+        
+
+# --- 3. Структура API ---
+
+# Список доступных вопросов (Queries)
 @strawberry.type
 class Query:
 
@@ -55,8 +109,25 @@ class Query:
     # Новое поле products.
     # Мы говорим: "Это поле вернет СПИСОК (List) объектов ProductType".
     # resolver=get_products связывает это поле с функцией выше.
+    # Список всех товаров
     products: List[ProductType] = strawberry.field(resolver=get_products)
 
+    # Один конкретный товар
+    # Мы указываем resolver=get_product. Strawberry увидит аргумент product_id в функции
+    # и автоматически добавит его в схему API.
+    product: Optional[ProductType] = strawberry.field(resolver=get_product)
 
-# --- 4. Создаем схему ---
-schema = strawberry.Schema(query=Query)
+
+# Список доступных действий (Mutations)
+@strawberry.type
+class Mutation:
+    # Мы называем действие 'addProduct'. 
+    # Strawberry поймет, какие аргументы нужны, посмотрев на функцию create_product
+    add_product: ProductType = strawberry.field(resolver=create_product)
+
+
+
+
+# --- 4. Сборка Схемы ---
+# Важно: теперь передаем и query, и mutation
+schema = strawberry.Schema(query=Query, mutation=Mutation)
